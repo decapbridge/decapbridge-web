be42d1c9b9c990015fc88099b52abfdb0907d4c5
Alexandre Lotte <dev@alexlotte.ca>
2024-06-22 22:29:57 -0400

    Add good screenshot

--- src/components/core/App/index.tsx
+++ src/components/core/App/index.tsx
@@ -2,6 +2,7 @@ import "@mantine/core/styles.css";
 import "@mantine/code-highlight/styles.css";
 import "@mantine/spotlight/styles.css";
 import "@mantine/notifications/styles.css";
+// import '@mantine/carousel/styles.css';
 
 import React from "react";
 import { Center, MantineProvider } from "@mantine/core";
--- src/pages/index/Demo/index.tsx
+++ src/pages/index/Demo/index.tsx
@@ -1,13 +1,29 @@
-import { Paper, Image, Container } from "@mantine/core";
-import useColorSchemeToggle from "/src/hooks/useColorSchemeToggle";
+// import { useRef } from "react";
+import { Paper, Image, Container, Title, Stack } from "@mantine/core";
+// import { Carousel } from '@mantine/carousel';
+// import AutoHeight from 'embla-carousel-auto-height';
+// import useColorSchemeToggle from "/src/hooks/useColorSchemeToggle";
+
+// const images = [...Array(4).keys()]
 
 const Demo: React.FC = () => {
-  const { colorScheme } = useColorSchemeToggle()
+  // const { colorScheme } = useColorSchemeToggle()
+  // const autoheight = useRef(AutoHeight());
   return (
     <Container size="md">
-      <Paper withBorder shadow="xl" style={{ overflow: 'hidden' }}>
-        <Image src={`/demo/demo-${colorScheme}.png`} alt="DecapBridge demo" />
-      </Paper>
+      <Stack>
+        <Title order={4} ta="center">Demo screenshot:</Title>
+        <Paper withBorder shadow="xl" style={{ overflow: 'hidden' }}>
+          <Image src={`/demo/screen-grab.png`} alt="DecapBridge demo" m="auto" />
+          {/* <Carousel withIndicators plugins={[autoheight.current as any]}>
+          {images.map(id => (
+            <Carousel.Slide key={id} display="flex">
+              <Image src={`/demo/demo-light-${id + 1}.png`} alt="DecapBridge demo" m="auto" />
+            </Carousel.Slide>
+          ))}
+        </Carousel> */}
+        </Paper>
+      </Stack>
     </Container>
   );
 };
