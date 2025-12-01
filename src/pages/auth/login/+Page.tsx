07493b825e84ab394c727a2e58c3a4a399ac8a12
Alexandre Lotte <dev@alexlotte.ca>
2025-12-01 15:57:16 -0500

    Remove redundant login form component

--- src/pages/auth/login/+Page.tsx
+++ src/pages/auth/login/+Page.tsx
@@ -1,20 +1,61 @@
-import { Title, Text, Group, Stack, Anchor } from "@mantine/core";
+import {
+  Title,
+  Text,
+  Group,
+  Stack,
+  Anchor,
+  Button,
+  Divider,
+  PasswordInput,
+  TextInput,
+} from "@mantine/core";
 import { useData } from "vike-react/useData";
 
 import InternalLink from "/src/components/core/InternalLink";
 import type { Data } from "./+data";
-import LoginForm from "/src/components/misc/LoginForm";
 import { usePageContext } from "vike-react/usePageContext";
+import {
+  TbBrandGoogleFilled,
+  TbBrandWindowsFilled,
+  TbAt,
+  TbLock,
+} from "react-icons/tb";
+import useAsyncForm, { FormWrapper } from "/src/hooks/useAsyncForm";
+import { useQueryClient } from "@tanstack/react-query";
+import usePkceAuth from "/src/hooks/usePkceAuth";
+import directus from "/src/utils/directus";
+import z from "zod";
+
+const schema = z.object({
+  email: z.email().max(255),
+  password: z.string().min(3).max(255),
+});
 
 const LoginPage: React.FC = () => {
   const content = useData<Data>();
 
   const { urlParsed } = usePageContext();
 
-  const isPkceFlow =
-    urlParsed.search["site_id"] &&
-    urlParsed.search["state"] &&
-    urlParsed.search["redirect_uri"];
+  const queryClient = useQueryClient();
+
+  const { isPkceFlow, getSsoRedirectUrl } = usePkceAuth();
+
+  const form = useAsyncForm({
+    allowMultipleSubmissions: true,
+    loadingOverlay: true,
+    schema,
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    action: async ({ email, password }) => {
+      await directus.login({ email, password }, { mode: "json" });
+      await queryClient.invalidateQueries({
+        queryKey: ["user"],
+        refetchType: "all",
+      });
+    },
+  });
 
   return (
     <Stack m="auto" maw={420} gap={0}>
@@ -40,7 +81,85 @@ const LoginPage: React.FC = () => {
           </Anchor>
         </Group>
       )}
-      <LoginForm />
+
+      <FormWrapper
+        form={form}
+        withBorder
+        shadow="md"
+        p="xl"
+        my="md"
+        radius="lg"
+      >
+        <Stack>
+          <Group justify="center">
+            <Button
+              leftSection={<TbBrandGoogleFilled size="1.25rem" />}
+              radius="xl"
+              variant="default"
+              component="a"
+              href={getSsoRedirectUrl("google")}
+            >
+              Google
+            </Button>
+            <Button
+              leftSection={<TbBrandWindowsFilled size="1.25rem" />}
+              radius="xl"
+              variant="default"
+              component="a"
+              href={getSsoRedirectUrl("microsoft")}
+            >
+              Microsoft
+            </Button>
+          </Group>
+
+          <Divider
+            label="Or continue with email"
+            labelPosition="center"
+            my="sm"
+          />
+          <TextInput
+            name="email"
+            label={content.email.label}
+            placeholder={content.email.placeholder}
+            leftSection={<TbAt size={16} />}
+            required
+            {...form.getInputProps("email")}
+            autoFocus
+          />
+          <Group pos="relative">
+            <PasswordInput
+              w="100%"
+              name="password"
+              label={content.password.label}
+              placeholder={content.password.placeholder}
+              leftSection={<TbLock size={16} />}
+              required
+              {...form.getInputProps("password")}
+            />
+            <Anchor
+              component={InternalLink}
+              href="/auth/password/forgot"
+              size="xs"
+              pos="absolute"
+              top={4}
+              right={0}
+            >
+              {content.forgot_password}
+            </Anchor>
+          </Group>
+          {form.errors.action && (
+            <Group justify="center">{form.errors.action}</Group>
+          )}
+          <Button
+            {...form.submitButtonProps}
+            fullWidth
+            accessKey="s"
+            mt={form.errors.action ? 0 : "xs"}
+          >
+            {isPkceFlow ? content.login_pkce_button : content.login_button}
+          </Button>
+        </Stack>
+      </FormWrapper>
     </Stack>
   );
 };
